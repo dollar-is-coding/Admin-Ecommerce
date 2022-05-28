0da62b023cf52d51f16db791361e28b346c35918
Dinh <0306201403@caothang.edu.vn>
2022-05-28 22:15:28 +0700

    index+login

---
+++ content/indexBody.php
@@ -0,0 +1,11 @@
+<div class="container">
+            <div class="row">
+                <div class="col">
+                    <p class="text-white mt-5 mb-5">Welcome back, <b>Admin</b></p>
+                </div>
+            </div>
+
+            <!-- mainIndexBody -->
+            <?php include content_path.body_path.'mainIndexBody.php'; ?>
+            
+        </div>
