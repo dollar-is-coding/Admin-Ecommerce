53722aff49130774be371d2c7e795c74a75a88f4
Luu Hang Nga <0306201354@caothang.edu.vn>
2022-05-28 22:31:17 +0700

    bai lam cua Nga(Account-Product)

---
+++ content/accountsBody.php
@@ -0,0 +1,8 @@
+<div class="container mt-5"> 
+
+        <!-- listOfAccounts -->
+        <?php include body_path.'listOfAcounts.php';?>
+
+        <!-- detailAccount -->
+        <?php include body_path.'detailAccount.php'; ?>
+</div>
---
+++ content/script/accountScript.php
@@ -0,0 +1,4 @@
+<script src="js/jquery-3.3.1.min.js"></script>
+    <!-- https://jquery.com/download/ -->
+    <script src="js/bootstrap.min.js"></script>
+    <!-- https://getbootstrap.com/ -->
